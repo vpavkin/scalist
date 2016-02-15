10285ba82b0448e7668f4206fa02b5f0ff0890a7
v.pavkin <vpavkin@gmail.com>
2016-02-15 23:11:02 +0300

    Rename plain request definition classes

--- core/src/main/scala/ru/pavkin/todoist/api/core/plain/PlainAPI.scala
+++ core/src/main/scala/ru/pavkin/todoist/api/core/plain/PlainAPI.scala
@@ -15,10 +15,10 @@ trait PlainAPI[F[_], Req, Base] extends API[F, Id, Base] {
   def get[R](implicit
              IR: IsResource[R],
              parser: SingleResourceParser.Aux[Id, Base, R]): SingleReadResourceDefinition[F, Id, R, Base] =
-    new PlainSingleReadResourceDefinition[F, R, Req, Base](requestFactory, executor)
+    new PlainSingleRequestDefinition[F, R, Req, Base](requestFactory, executor)
 
   def getAll[R <: HList](implicit
                          IR: IsResource[R],
                          parser: Aux[Id, Base, R]): MultipleReadResourceDefinition[F, Id, R, Base] =
-    new PlainMultipleReadResourceDefinition[F, R, Req, Base](requestFactory, executor)
+    new PlainMultipleRequestDefinition[F, R, Req, Base](requestFactory, executor)
 }
