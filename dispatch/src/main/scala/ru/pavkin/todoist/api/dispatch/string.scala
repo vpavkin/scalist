975e1be5f9bd0b9e5d1d2fbe19ea94c3dc3f7ac1
v.pavkin <vpavkin@gmail.com>
2016-02-16 17:49:11 +0300

    Add dispatch.string API tests

--- dispatch/src/main/scala/ru/pavkin/todoist/api/dispatch/string.scala
+++ dispatch/src/main/scala/ru/pavkin/todoist/api/dispatch/string.scala
@@ -6,7 +6,6 @@ import ru.pavkin.todoist.api._
 import ru.pavkin.todoist.api.core._
 import ru.pavkin.todoist.api.core.plain.PlainAPISuite
 import ru.pavkin.todoist.api.dispatch.core.DispatchAuthorizedRequestFactory
-import ru.pavkin.todoist.api.dispatch.impl.string.DispatchStringRequestExecutor.Result
 import ru.pavkin.todoist.api.dispatch.impl.string.{DispatchStringAPI, DispatchStringRequestExecutor}
 
 object string extends PlainAPISuite[String] {
@@ -15,7 +14,7 @@ object string extends PlainAPISuite[String] {
     private lazy val executor: RequestExecutor.Aux[Req, DispatchStringRequestExecutor.Result, String] =
       new DispatchStringRequestExecutor
 
-    def authorize(token: Token): API[Result, Id, String] =
+    def authorize(token: Token): API[DispatchStringRequestExecutor.Result, Id, String] =
       new DispatchStringAPI(
         new DispatchAuthorizedRequestFactory(token),
         executor
