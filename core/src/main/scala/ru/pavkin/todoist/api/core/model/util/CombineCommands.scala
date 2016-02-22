5faf5318901719f9fc335f544259bba233a47b50
v.pavkin <vpavkin@gmail.com>
2016-02-22 17:15:50 +0300

    Implement command combination without a type class

--- core/src/main/scala/ru/pavkin/todoist/api/core/model/util/CombineCommands.scala
+++ core/src/main/scala/ru/pavkin/todoist/api/core/model/util/CombineCommands.scala
@@ -3,17 +3,10 @@ package ru.pavkin.todoist.api.core.model.util
 import ru.pavkin.todoist.api.core.model.Command
 import shapeless.{::, HNil}
 
-trait CombineCommands[C] {
-  def and[A](command: C, other: A): C :: A :: HNil =
-    command :: other :: HNil
-}
-
 object CombineCommands {
   trait Syntax {
-    implicit class CombineCommandsOps[C](command: C)(implicit C: CombineCommands[C]) {
-      def :+[A](other: A): C :: A :: HNil = C.and(command, other)
+    implicit class CombineCommandsOps[C <: Command](command: C) {
+      def :+[A](other: A): C :: A :: HNil = command :: other :: HNil
     }
   }
-
-  implicit def commandsCombine[C <: Command]: CombineCommands[C] = new CombineCommands[C] {}
 }
