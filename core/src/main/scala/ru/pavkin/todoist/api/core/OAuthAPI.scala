b0a803b0b44671d7bfcc6079136f855d0c4ff9a1
v.pavkin <vpavkin@gmail.com>
2016-03-05 14:10:14 +0300

    Add OAuthAPI scaladocs

--- core/src/main/scala/ru/pavkin/todoist/api/core/OAuthAPI.scala
+++ core/src/main/scala/ru/pavkin/todoist/api/core/OAuthAPI.scala
@@ -4,15 +4,30 @@ import ru.pavkin.todoist.api._
 import ru.pavkin.todoist.api.core.decoder.SingleResponseDecoder
 import ru.pavkin.todoist.api.core.model.{TokenExchange, TokenScope}
 
+/**
+  * Supplies helper methods for Todoist authorization API
+  */
 trait OAuthAPI[F[_], P[_], Base] {
 
+  /**
+    * Returns an oAuth step 3 request definition, that upon execution
+    * exchanges security code on API token.
+    *
+    * @param request Exchange request parameters
+    */
   def oAuthStep3(request: TokenExchange)
                 (implicit
                  trr: ToRawRequest[TokenExchange],
                  parser: SingleResponseDecoder[P, Base, model.AccessToken])
   : RequestDefinition[F, P, model.AccessToken, Base]
 
-
+  /**
+    * Constructs a url for oAuth step 1 request, based on supplied parameters
+    *
+    * @param clientId client id
+    * @param scopes   a set of scopes that are required by authorizing application
+    * @param state    unique state parameter
+    */
   def oAuthStep1URL(clientId: String, scopes: Set[TokenScope], state: String): String =
     s"$oAuthURL?" + List(
       "client_id" -> clientId,
